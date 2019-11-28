a2142b2fca97dd8e8a38e54dae2aa68778f5e9e5
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-11-28 15:42:27 +0200

    start

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // main big slider
+    $('.bigslider').slick({
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        arrows: true,
+        // vertical: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        dots: true,
+        infinite: true,
+        fade: true,
+        speed: 1400,
+    });
+    // main big slider end
+
+});
