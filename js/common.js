3c84d9f44c4688aec2007674b0792fd418e9109f
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-11-28 16:04:29 +0200

    test

--- js/common.js
+++ js/common.js
@@ -14,4 +14,25 @@ $(document).ready(function () {
     });
     // main big slider end
 
+    // label animation
+    (function ($) {
+        function floatLabel(inputType) {
+            $(inputType).each(function () {
+                var $this = $(this);
+                // on focus add cladd active to label
+                $this.focus(function () {
+                    $this.next().addClass("active");
+                });
+                //on blur check field and remove class if needed
+
+                $this.blur(function () {
+                    if ($this.val() === '') {
+                        $this.next().removeClass();
+                    }
+                });
+            });
+        }
+        // just add a class of "floatLabel to the input field!"
+        floatLabel(".floatLabel");
+    })(jQuery);
 });
