7ef41e95b6b8b628f72be74b883ceec517915c05
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-12-02 17:49:36 +0200

    remove slider arrow

--- js/common.js
+++ js/common.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
     // main big slider
     $('.bigslider').slick({
-        // slidesToShow: 1,
-        // slidesToScroll: 1,
-        arrows: true,
-        // vertical: true,
+        arrows: false,
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
         dots: true,
